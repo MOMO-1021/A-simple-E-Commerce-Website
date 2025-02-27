00b0e9e029e5cd99ec6d12f6f379f23e79495533
MOMO-1021 <92034273+MOMO-1021@users.noreply.github.com>
2025-02-27 18:20:24 +0530

    Add files via upload

---
+++ Ecommerce Website/script.js
@@ -0,0 +1,289 @@
+'use strict';
+const coats = document.querySelector('.coat');
+const coatsContainer = document.querySelector('.Coats');
+// console.log(coatsContainer);
+coats.addEventListener('click', () => {
+    coatsContainer.style.display = "block";
+    shoesContainer.style.display = "none";
+    trousersContainer.style.display = "none";
+    sunglassesContainer.style.display = "none";
+    beltsContainer.style.display = "none";
+    watchContainer.style.display = "none";
+    hatContainer.style.display = "none";
+    sandalContainer.style.display = "none";
+    sareeContainer.style.display = "none";
+    lungiContainer.style.display = "none";
+    frockContainer.style.display = "none";
+    gownContainer.style.display = "none";
+    document.querySelector('.tutorialVideo').style.display = 'none';
+    document.querySelector('.hint p').style.display = 'none';
+    document.querySelector('.hint .right').style.display = 'none';
+    document.querySelector('.hint .left').style.display = 'none';
+})
+const shoes = document.querySelector('.shoes');
+const shoesContainer = document.querySelector('.Shoes');
+shoes.addEventListener('click', () => {
+    shoesContainer.style.display = "block";
+    coatsContainer.style.display = "none";
+    trousersContainer.style.display = "none";
+    sunglassesContainer.style.display = "none";
+    beltsContainer.style.display = "none";
+    watchContainer.style.display = "none";
+    hatContainer.style.display = "none";
+    sandalContainer.style.display = "none";
+    sareeContainer.style.display = "none";
+    lungiContainer.style.display = "none";
+    frockContainer.style.display = "none";
+    gownContainer.style.display = "none";
+    document.querySelector('.tutorialVideo').style.display = 'none';
+    document.querySelector('.hint p').style.display = 'none';
+    document.querySelector('.hint .right').style.display = 'none';
+    document.querySelector('.hint .left').style.display = 'none';
+})
+const trousers = document.querySelector('.trousers');
+const trousersContainer = document.querySelector('.Trousers');
+trousers.addEventListener('click', () => {
+    trousersContainer.style.display = "block";
+    coatsContainer.style.display = "none";
+    shoesContainer.style.display = "none";
+    sunglassesContainer.style.display = "none";
+    beltsContainer.style.display = "none";
+    watchContainer.style.display = "none";
+    hatContainer.style.display = "none";
+    sandalContainer.style.display = "none";
+    sareeContainer.style.display = "none";
+    lungiContainer.style.display = "none";
+    frockContainer.style.display = "none";
+    gownContainer.style.display = "none";
+    document.querySelector('.tutorialVideo').style.display = 'none';
+    document.querySelector('.hint p').style.display = 'none';
+    document.querySelector('.hint .right').style.display = 'none';
+    document.querySelector('.hint .left').style.display = 'none';
+})
+const sunglasses = document.querySelector('.sunglasses');
+const sunglassesContainer = document.querySelector('.Sunglasses');
+sunglasses.addEventListener('click', () => {
+    sunglassesContainer.style.display = "block";
+    trousersContainer.style.display = "none";
+    coatsContainer.style.display = "none";
+    shoesContainer.style.display = "none";
+    beltsContainer.style.display = "none";
+    watchContainer.style.display = "none";
+    hatContainer.style.display = "none";
+    sandalContainer.style.display = "none";
+    sareeContainer.style.display = "none";
+    lungiContainer.style.display = "none";
+    frockContainer.style.display = "none";
+    gownContainer.style.display = "none";
+    document.querySelector('.tutorialVideo').style.display = 'none';
+    document.querySelector('.hint p').style.display = 'none';
+    document.querySelector('.hint .right').style.display = 'none';
+    document.querySelector('.hint .left').style.display = 'none';
+})
+const belts = document.querySelector('.belts');
+const beltsContainer = document.querySelector('.Belts');
+belts.addEventListener('click', () => {
+    beltsContainer.style.display = "block";
+    sunglassesContainer.style.display = "none";
+    trousersContainer.style.display = "none";
+    coatsContainer.style.display = "none";
+    shoesContainer.style.display = "none";
+    watchContainer.style.display = "none";
+    hatContainer.style.display = "none";
+    sandalContainer.style.display = "none";
+    sareeContainer.style.display = "none";
+    lungiContainer.style.display = "none";
+    frockContainer.style.display = "none";
+    gownContainer.style.display = "none";
+    document.querySelector('.tutorialVideo').style.display = 'none';
+    document.querySelector('.hint p').style.display = 'none';
+    document.querySelector('.hint .right').style.display = 'none';
+    document.querySelector('.hint .left').style.display = 'none';
+})
+const watch = document.querySelector('.watches');
+const watchContainer = document.querySelector('.Watches');
+watch.addEventListener('click', () => {
+    watchContainer.style.display = "block";
+    beltsContainer.style.display = "none";
+    sunglassesContainer.style.display = "none";
+    trousersContainer.style.display = "none";
+    coatsContainer.style.display = "none";
+    shoesContainer.style.display = "none";
+    hatContainer.style.display = "none";
+    sandalContainer.style.display = "none";
+    sareeContainer.style.display = "none";
+    lungiContainer.style.display = "none";
+    frockContainer.style.display = "none";
+    gownContainer.style.display = "none";
+    document.querySelector('.tutorialVideo').style.display = 'none';
+    document.querySelector('.hint p').style.display = 'none';
+    document.querySelector('.hint .right').style.display = 'none';
+    document.querySelector('.hint .left').style.display = 'none';
+})
+const hat = document.querySelector('.hats');
+const hatContainer = document.querySelector('.Hats');
+hat.addEventListener('click', () => {
+    hatContainer.style.display = "block";
+    watchContainer.style.display = "none";
+    beltsContainer.style.display = "none";
+    sunglassesContainer.style.display = "none";
+    trousersContainer.style.display = "none";
+    coatsContainer.style.display = "none";
+    shoesContainer.style.display = "none";
+    sandalContainer.style.display = "none";
+    sareeContainer.style.display = "none";
+    lungiContainer.style.display = "none";
+    frockContainer.style.display = "none";
+    gownContainer.style.display = "none";
+    document.querySelector('.tutorialVideo').style.display = 'none';
+    document.querySelector('.hint p').style.display = 'none';
+    document.querySelector('.hint .right').style.display = 'none';
+    document.querySelector('.hint .left').style.display = 'none';
+})
+const sandal = document.querySelector('.sandals');
+const sandalContainer = document.querySelector('.Sandals');
+sandal.addEventListener('click', () => {
+    sandalContainer.style.display = "block";
+    hatContainer.style.display = "none";
+    watchContainer.style.display = "none";
+    beltsContainer.style.display = "none";
+    sunglassesContainer.style.display = "none";
+    trousersContainer.style.display = "none";
+    coatsContainer.style.display = "none";
+    shoesContainer.style.display = "none";
+    sareeContainer.style.display = "none";
+    lungiContainer.style.display = "none";
+    frockContainer.style.display = "none";
+    gownContainer.style.display = "none";
+    document.querySelector('.tutorialVideo').style.display = 'none';
+    document.querySelector('.hint p').style.display = 'none';
+    document.querySelector('.hint .right').style.display = 'none';
+    document.querySelector('.hint .left').style.display = 'none';
+})
+const saree = document.querySelector('.sarees');
+const sareeContainer = document.querySelector('.Sarees');
+saree.addEventListener('click', () => {
+    sareeContainer.style.display = "block";
+    sandalContainer.style.display = "none";
+    hatContainer.style.display = "none";
+    watchContainer.style.display = "none";
+    beltsContainer.style.display = "none";
+    sunglassesContainer.style.display = "none";
+    trousersContainer.style.display = "none";
+    coatsContainer.style.display = "none";
+    shoesContainer.style.display = "none";
+    lungiContainer.style.display = "none";
+    frockContainer.style.display = "none";
+    gownContainer.style.display = "none";
+    document.querySelector('.tutorialVideo').style.display = 'none';
+    document.querySelector('.hint p').style.display = 'none';
+    document.querySelector('.hint .right').style.display = 'none';
+    document.querySelector('.hint .left').style.display = 'none';
+})
+const lungi = document.querySelector('.lungi');
+const lungiContainer = document.querySelector('.Lungi');
+lungi.addEventListener('click', () => {
+    lungiContainer.style.display = "block";
+    sandalContainer.style.display = "none";
+    hatContainer.style.display = "none";
+    watchContainer.style.display = "none";
+    beltsContainer.style.display = "none";
+    sunglassesContainer.style.display = "none";
+    trousersContainer.style.display = "none";
+    coatsContainer.style.display = "none";
+    shoesContainer.style.display = "none";
+    sareeContainer.style.display = "none";
+    frockContainer.style.display = "none";
+    gownContainer.style.display = "none";
+    document.querySelector('.tutorialVideo').style.display = 'none';
+    document.querySelector('.hint p').style.display = 'none';
+    document.querySelector('.hint .right').style.display = 'none';
+    document.querySelector('.hint .left').style.display = 'none';
+})
+const gown = document.querySelector('.gowns');
+const gownContainer = document.querySelector('.Gowns');
+gown.addEventListener('click', () => {
+    gownContainer.style.display = "block";
+    frockContainer.style.display = "none";
+    lungiContainer.style.display = "none";
+    sandalContainer.style.display = "none";
+    hatContainer.style.display = "none";
+    watchContainer.style.display = "none";
+    beltsContainer.style.display = "none";
+    sunglassesContainer.style.display = "none";
+    trousersContainer.style.display = "none";
+    coatsContainer.style.display = "none";
+    shoesContainer.style.display = "none";
+    sareeContainer.style.display = "none";
+    document.querySelector('.tutorialVideo').style.display = 'none';
+    document.querySelector('.hint p').style.display = 'none';
+    document.querySelector('.hint .right').style.display = 'none';
+    document.querySelector('.hint .left').style.display = 'none';
+})
+const frock = document.querySelector('.frocks');
+const frockContainer = document.querySelector('.Frocks');
+frock.addEventListener('click', () => {
+    frockContainer.style.display = "block";
+    sandalContainer.style.display = "none";
+    hatContainer.style.display = "none";
+    watchContainer.style.display = "none";
+    beltsContainer.style.display = "none";
+    sunglassesContainer.style.display = "none";
+    trousersContainer.style.display = "none";
+    coatsContainer.style.display = "none";
+    shoesContainer.style.display = "none";
+    sareeContainer.style.display = "none";
+    gownContainer.style.display = "none";
+    lungiContainer.style.display = "none";
+    document.querySelector('.tutorialVideo').style.display = 'none';
+    document.querySelector('.hint p').style.display = 'none';
+    document.querySelector('.hint .right').style.display = 'none';
+    document.querySelector('.hint .left').style.display = 'none';
+})
+const shopping = document.querySelector('.main');
+const logo = document.querySelector('.logo');
+logo.addEventListener('mouseover', ()=> {
+    document.body.style.background = "linear-gradient(to right, #1b7d40 , #aea495, #7171b5)";
+    shopping.style.textShadow = "2px 2px 3px blue";
+    
+});
+// Remember that you cannot use linear gradient using background Color but Background
+logo.addEventListener('mouseout', ()=> {
+    document.body.style.background = "linear-gradient(to left, #1b7d40 , #aea495, #7171b5)";
+    shopping.style.textShadow = "none";
+})
+const video = document.querySelector('.tutorialVideo video');
+video.muted = true; // true is a value so "" is not needed.
+//          Notes to be kept in mind while playing a video on a website:
+// Many browsers have strict autoplay policies that prevent videos with sound from 
+// playing automatically unless they are muted or initiated by user interaction. If your 
+// video has sound, try muting it initially
+video.addEventListener('mouseover', () => {
+     video.play();
+     video.style.cursor = 'pointer';
+})
+video.addEventListener('mouseout', () => {
+    video.pause();
+    video.muted = true;
+})
+// video.addEventListener('onClick', ()=> {
+//     if(video.muted){
+//         video.muted = false;
+//         video.play();
+//     }else{
+//         video.pause();
+//         video.muted = true;
+//     }
+// }) This does not work, i don't know why!
+const itemList = document.querySelector('.itemList');
+const hiddenContent = document.querySelector('.hiddenContent');
+const displayButton = document.querySelector('.displayButton');
+displayButton.addEventListener('click', ()=>{
+    itemList.style.display = 'block';
+    hiddenContent.style.display = 'none';
+    document.querySelector('.tutorialVideo').style.display = 'block';
+    document.querySelector('.hint p').style.display = 'block';
+    document.querySelector('.hint .right').style.display = 'block';
+    document.querySelector('.hint .left').style.display = 'block';
+    document.querySelector('footer').style.display = 'block';
+})
